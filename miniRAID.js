f77270e6eb8428a84227f6e5ce24ddf8f5865533
Betairya <betairya@gmail.com>
2018-06-28 03:18:03 +0900

    Finished tilemap rendering with sprites need changes. It will produce artifacts (gaps between sprites), If turn AA on or zoom in. And huge performance cost.

--- miniRAID.js
+++ miniRAID.js
@@ -1,6 +1,6 @@
 //miniRAID main script
 var canvas = document.getElementById("gameMainCanvas"); // Get the canvas element
-var engine = new BABYLON.Engine(canvas, true); // Generate the BABYLON 3D engine
+var engine = new BABYLON.Engine(canvas, false); // Generate the BABYLON 3D engine
 
 var gameApp = gameApp || {};
 
@@ -15,7 +15,9 @@ gameApp =
     {
         // Create the scene space
         var scene = new BABYLON.Scene(engine);
-
+//
+// ─── ORTHO 2D CAMERA ────────────────────────────────────────────────────────────
+//
         // Add a camera to the scene and attach it to the canvas
         // It is an orthographic camera, as a 2d game.
         var camera = new BABYLON.FreeCamera("FreeCamera", new BABYLON.Vector3(0, 0, -10), scene);
@@ -29,10 +31,20 @@ gameApp =
         camera.orthoBottom = -9;
         camera.orthoLeft = -16;
         camera.orthoRight = 16;
+//
+// ─── FREE CAMERA ────────────────────────────────────────────────────────────────
+//
+        // // Parameters : name, position, scene
+        // var camera = new BABYLON.UniversalCamera("UniversalCamera", new BABYLON.Vector3(0, 0, -10), scene);
 
+        // // Targets the camera to a particular position. In this case the scene origin
+        // camera.setTarget(BABYLON.Vector3.Zero());
+        
+        // // Attach the camera to the canvas
+        // camera.attachControl(canvas, true);
+// ────────────────────────────────────────────────────────────────────────────────
         // Add lights to the scene
-        var light2 = new BABYLON.PointLight("light2", new BABYLON.Vector3(0, 0, 0), scene);
-
+        var light2 = new BABYLON.PointLight("light2", new BABYLON.Vector3(0, 0, 0), scene);        
         // Add and manipulate meshes in the scene
         sphere = BABYLON.MeshBuilder.CreateSphere("sphere", {diameter:2}, scene);
         sphere2 = BABYLON.MeshBuilder.CreateSphere("sphere", {diameter:2}, scene);
@@ -55,8 +67,74 @@ gameApp =
         this.sprite1 = new BABYLON.Sprite("test1", this.testSpritePool.getMgr("test", this.scene, 16));
         this.sprite2 = new BABYLON.Sprite("test2", this.testSpritePool.getMgr("test", this.scene, 16));
 
+        // grouping test
+        this.sprite1.parent = this.sprite2;
+
         // change the first sprite to second cell
         this.sprite1.cellIndex = 1;
+
+        // Generate a temp world lol
+        tmp = new Array(576);
+        var _x, _y;
+        for(_x = 0; _x < 32; _x ++)
+        {
+            for(_y = 0; _y < 18; _y ++)
+            {
+                var id = _x * 18 + _y;
+
+                if(_y < 8)
+                {
+                    if(_x < 5 || _x > 27)
+                    {
+                        tmp[id] = 0;
+                    }
+                    else if(_x == 5)
+                    {
+                        tmp[id] = 4;
+                    }
+                    else if(_x == 27)
+                    {
+                        tmp[id] = 6;
+                    }
+                    else
+                    {
+                        if(getRandomFloat(0, 1) < 0.1)
+                        {
+                            tmp[id] = 7;
+                        }
+                        else
+                        {
+                            tmp[id] = 5;
+                        }
+                    }
+                }
+                else if (_y == 8)
+                {
+                    if(_x < 5 || _x > 27)
+                    {
+                        tmp[id] = 0;
+                    }
+                    else if(_x == 5)
+                    {
+                        tmp[id] = 2;
+                    }
+                    else if(_x == 27)
+                    {
+                        tmp[id] = 3;
+                    }
+                    else
+                    {
+                        tmp[id] = 1;
+                    }
+                }
+                else
+                {
+                    tmp[id] = 0;
+                }
+            }
+        }
+
+        this.map = new TiledMap({name: "test", isSolid: tmp}, tmp, this.scene);
     },
 
     //
@@ -68,8 +146,8 @@ gameApp =
         //Calculate total time
         this.timeTotal += deltaTime;
 
-        this.sprite1.position = (new BABYLON.Vector3(Math.sin(this.timeTotal), Math.cos(this.timeTotal), 0)).scale(6);
-        this.sprite2.position = (new BABYLON.Vector3(Math.cos(this.timeTotal), Math.sin(this.timeTotal), 0)).scale(4);
+        this.sprite1.position = (new BABYLON.Vector3(Math.sin(this.timeTotal), Math.cos(this.timeTotal), -1)).scale(6);
+        this.sprite2.position = (new BABYLON.Vector3(Math.cos(this.timeTotal), Math.sin(this.timeTotal), -1)).scale(4);
     },
 
     //
